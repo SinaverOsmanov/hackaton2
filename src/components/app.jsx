d1fd44afe83488e38a437db2d2125852e9e1ea99
vulpies <anonim616@gmail.com>
2021-10-09 16:34:06 +0300

    навигация

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React from "react"
-=======
 import React, { useEffect } from "react"
->>>>>>> 9a5fcb366542d354b753d5e262251fd8bde9acfc
 import Header from "./header"
 import Content from "./content"
 import { setUserLocalStorage } from "../storageAPI/api"
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,9 +3,9 @@ import NavBar from "./navBar/navBar"
 
 const Header = () => {
     return (
-        <div>
+        <>
             <NavBar />
-        </div>
+        </>
     )
 }
 
