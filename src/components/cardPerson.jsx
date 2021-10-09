d5896a0061474c24aaf3421a42f77183dddaf4f3
Osmanov <sinaverosmanovfrontend@gmail.com>
2021-10-09 20:38:21 +0300

    onlyfans

--- src/components/cardPerson.jsx
+++ src/components/cardPerson.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {Button, Card, Col} from "antd"
+import { Button, Card, Col } from "antd"
 import { Link } from "react-router-dom"
 
 const CardPerson = ({user}) => {
