015e5f78ae7ee05094a9d344c9226490ad400ae6
Denis Bugreev <den9.rcq@gmail.com>
2021-10-09 14:53:41 +0300

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Routers from "./routers"
+import Header from "./header"
+import Content from "./content"
+
+function App() {
+    return (
+        <div className="container">
+            <Header/>
+            <Content/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/pages/favoritesPage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Favorites = () => {
+    return (
+        <div>
+            SINAVER DELAET NE LEZEM!
+        </div>
+    )
+}
+
+export default Favorites
