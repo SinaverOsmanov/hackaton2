89d6ddc35fa87fb6731478d4785ce0f11f68da55
vulpies <anonim616@gmail.com>
2021-10-09 17:37:47 +0300

    updated navigation

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -4,19 +4,23 @@ import style from "./navBar.module.css"
 
 const NavBar = () => {
     return (
-        <nav className={style.menu}>
-            <li className={style.item}>
-                <Link to="/">
-                    <button className={`${style.btn} btn`}>Главная</button>
-                </Link>
-            </li>
+        <div className={style.menu}>
+            <div className={style.wrapper}>
+                <li className={style.item}>
+                    <Link to="/">
+                        <button className={`${style.btn} btn`}>Главная</button>
+                    </Link>
+                </li>
 
-            <li className={style.item}>
-                <Link to="/favorites">
-                    <button className={`${style.btn} btn`}>Избранное</button>
-                </Link>
-            </li>
-        </nav>
+                <li className={style.item}>
+                    <Link to="/favorites">
+                        <button className={`${style.btn} btn`}>
+                            Избранное
+                        </button>
+                    </Link>
+                </li>
+            </div>
+        </div>
     )
 }
 
