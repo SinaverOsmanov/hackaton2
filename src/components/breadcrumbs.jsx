818e379a046c8ae75a17b8f96cef91c416550d61
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2021-10-10 20:28:08 +0300

    add button  proptypes

--- src/components/breadcrumbs.jsx
+++ src/components/breadcrumbs.jsx
@@ -5,12 +5,13 @@ import { Breadcrumb } from "antd"
 const BreadcrumbsComponent = () => {
     const { pathname } = useLocation()
     if (pathname === "/") return null
-    let title2 = pathname.split("/").filter(e => e !== "")
-    title2 = title2.filter(e => e !== "users")
+    let title = pathname.split("/").filter(e => e !== "")
+    title = title.filter(e => e !== "users")
+    title = title.filter(e => e !== "error")
     return (
         <Breadcrumb>
             <Breadcrumb.Item ><Link to="/">Main</Link></Breadcrumb.Item>
-            {title2.map(t => (<Breadcrumb.Item key={t}><Link to={t}>{t = t[0].toUpperCase() + t.slice(1)}</Link></Breadcrumb.Item>))}
+            {title.map(t => (<Breadcrumb.Item key={t}><Link to={t}>{t = t[0].toUpperCase() + t.slice(1)}</Link></Breadcrumb.Item>))}
         </Breadcrumb>
     )
 }
