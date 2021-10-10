3f13aebf494cc4c3ed07f39d80dd99c3dd2f8700
vulpies <anonim616@gmail.com>
2021-10-10 18:07:07 +0300

    bckg image, styles

--- src/components/progress.jsx
+++ src/components/progress.jsx
@@ -1,32 +1,40 @@
 import React from "react"
 import { Progress } from "antd"
-import {logDOM} from "@testing-library/react"
 
 const ProgressBar = ({ skillName, type, color, percents }) => {
     const changeColor = (name) => {
         let colorBar
         switch (name) {
-        case "HTML":
-            colorBar = "#fa541c"
-            break
-        case "CSS":
-            colorBar = "#531dab"
-            break
-        case "React":
-            colorBar = "#1890ff"
-            break
-        default:
-            break
+            case "HTML":
+                colorBar = "#fa541c"
+                break
+            case "CSS":
+                colorBar = "#531dab"
+                break
+            case "React":
+                colorBar = "#1890ff"
+                break
+            default:
+                break
         }
         return colorBar
     }
     return (
         <>
-            <div style={{textAlign:"center"}}>
+            <div style={{ textAlign: "left" }}>
                 <p>{skillName}</p>
-                {type === "circle"
-                    ? <Progress type={type} percent={percents} strokeColor={color || changeColor(skillName)} />
-                    : <Progress percent={percents} strokeColor={color || changeColor(skillName)} />}
+                {type === "circle" ? (
+                    <Progress
+                        type={type}
+                        percent={percents}
+                        strokeColor={color || changeColor(skillName)}
+                    />
+                ) : (
+                    <Progress
+                        percent={percents}
+                        strokeColor={color || changeColor(skillName)}
+                    />
+                )}
             </div>
         </>
     )
