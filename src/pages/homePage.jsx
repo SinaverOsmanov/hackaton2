af58fc60da27ee17a67f3cb240c336131f24fb86
Osmanov <sinaverosmanovfrontend@gmail.com>
2021-10-10 14:54:24 +0300

    small small loading fix

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -7,14 +7,14 @@ import { getUsersLocalStorage, setUsersLocalStorage } from "../storageAPI/api"
 const HomePage = ({callback}) => {
     const [users, setUsers] = useState(()=>{return getUsersLocalStorage()})
 
-    if(!users) return "loading"
-
     function clickFavoriteUserHandler(id){
         const mapUsers = users.map(u=> u.id === id ? {...u, favorite: !u.favorite}: u)
         setUsers(mapUsers)
         callback()
         setUsersLocalStorage(mapUsers)
     }
+    
+    if(!users) return "loading"
     
     return (
         <Row gutter={10} justify="center" className={style.common}>
