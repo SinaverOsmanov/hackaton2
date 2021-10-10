90c064bcd3eef5010b8ec9ef561fc697b064bb60
vulpies <anonim616@gmail.com>
2021-10-10 14:00:57 +0300

    no message

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,20 +7,22 @@ import style from "./app.module.css"
 
 function App() {
     const [state, setState] = useState(0)
-    function callback(){
-        setState(state+1)
+    function callback() {
+        setState(state + 1)
     }
     return (
-        <Layout className={style.bckg}>
+        <Layout className={style.bckg} style={{ backgroundSize: "cover" }}>
             <div className={style.container}>
                 <Header />
-                <ProgressBar
-                    skillName={"ivan"}
-                    color={"blue"}
-                    type={"bar"}
-                    percents={75}
-                />
-                <Content callback={callback}/>
+                <div className={style.comwrap}>
+                    <ProgressBar
+                        skillName={"ivan"}
+                        color={"blue"}
+                        type={"bar"}
+                        percents={75}
+                    />
+                    <Content callback={callback} />
+                </div>
             </div>
         </Layout>
     )
