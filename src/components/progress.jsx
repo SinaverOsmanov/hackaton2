3bb408f9c0b6c4cdb27007255420b7cf17c347ef
Denis Bugreev <den9.rcq@gmail.com>
2021-10-10 15:11:34 +0300

    refactoring changeColor

--- src/components/progress.jsx
+++ src/components/progress.jsx
@@ -3,13 +3,30 @@ import { Progress } from "antd"
 import {logDOM} from "@testing-library/react"
 
 const ProgressBar = ({ skillName, type, color, percents }) => {
+    const changeColor = (name) => {
+        let colorBar
+        switch (name) {
+        case "HTML":
+            colorBar = "#fa541c"
+            break
+        case "CSS":
+            colorBar = "#531dab"
+            break
+        case "React":
+            colorBar = "#1890ff"
+            break
+        default:
+            break
+        }
+        return colorBar
+    }
     return (
         <>
             <div style={{textAlign:"center"}}>
                 <p>{skillName}</p>
                 {type === "circle"
-                    ? <Progress type={type} percent={percents} strokeColor={color} />
-                    : <Progress percent={percents} strokeColor={color} />}
+                    ? <Progress type={type} percent={percents} strokeColor={color || changeColor(skillName)} />
+                    : <Progress percent={percents} strokeColor={color || changeColor(skillName)} />}
             </div>
         </>
     )
